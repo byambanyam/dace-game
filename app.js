b90d856c25644e6df4d25746c9b0cb7c16adc64b
Byambanyam <bymbanym22@gmail.com>
2021-03-15 19:11:22 +0800

    yrunhiidoo bolson tohiruulan

--- app.js
+++ app.js
@@ -10,10 +10,14 @@ document.getElementById("score-0").textContent=0;
 document.getElementById("score-1").textContent=0;
 document.getElementById("current-0").textContent=0;
 document.getElementById("current-1").textContent=0;
-
+ 
+   
+document.getElementById("name-0").textContent = "БЯМБАНЯМ";
+document.getElementById("name-1").textContent = "БЯМБАДОРЖ";
 var DiceDom=document.querySelector(".dice");
 DiceDom.style.display="none";
 document.querySelector(".btn-roll").addEventListener("click" , function (){
+    
     var diceNumber = Math.floor(Math.random() * 6)+1 ;
     DiceDom.style.display="block";
     DiceDom.src = "dice-"+diceNumber+".png";
@@ -25,7 +29,37 @@ if(diceNumber !== 1) {
 }
 
      else {
-          roundscore=0;
+        eeljSolihFunctoin();
+
+         DiceDom.style.display="none";
+        }});
+        document.querySelector(".btn-hold").addEventListener("click",function(){
+
+            scorce[Activplayer]=scorce[Activplayer] + roundscore
+            document.getElementById("score-"+Activplayer).textContent = scorce[Activplayer];
+            
+if(scorce[Activplayer] >= 10)
+{
+document.getElementById("name-"+Activplayer).textContent="!!!winner!!!"
+document.querySelector(".player-" + Activplayer +"-panel").classList.add('winner');
+document.querySelector(".player-" + Activplayer +"-panel").classList.remove("active");
+
+}
+else
+{
+    eeljSolihFunctoin();
+}
+
+           
+
+
+
+
+
+    })
+    //eelj solih functoin
+    function eeljSolihFunctoin(){
+        roundscore=0;
         document.getElementById("current-" + Activplayer).textContent = 0;
   
            Activplayer === 0 ? (Activplayer = 1) : (Activplayer = 0);
@@ -34,6 +68,9 @@ if(diceNumber !== 1) {
   
          document.querySelector(".player-1-panel").classList.toggle("active");
 
-         DiceDom.style.display="none";
-        }
-});
+    }
+    //new game iig tohiruulah
+    document.querySelector(".btn-new").addEventListener('click',function(){
+
+        
+    })
