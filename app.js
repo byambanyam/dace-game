c034f77bdf01a0e5feb7a5124f37f69c93819deb
Byambanyam <bymbanym22@gmail.com>
2021-03-15 13:08:17 +0800

    zasaw shoog ynzlaw

--- app.js
+++ app.js
@@ -2,21 +2,29 @@
 //onoog tsugluuldeg huwisagch 
 // eeljiin onoo huwsagch 
 //
-var Activplayer=1;
+var Activplayer=0;
 var scorce = [0 , 0];
-var roudscore=0 ;
+var roundscore=0 ;
 // var diceNumber =Math.floor(Math.random() * 6)+1; 
 document.getElementById("score-0").textContent=0;
 document.getElementById("score-1").textContent=0;
 document.getElementById("current-0").textContent=0;
 document.getElementById("current-1").textContent=0;
 
-var DiceDom=document.querySelector(".dice")
+var DiceDom=document.querySelector(".dice");
 DiceDom.style.display="none";
 document.querySelector(".btn-roll").addEventListener("click" , function (){
-    var diceNumber =Math.floor(Math.random() * 6)+1 ;
+    var diceNumber = Math.floor(Math.random() * 6)+1 ;
     DiceDom.style.display="block";
     DiceDom.src = "dice-"+diceNumber+".png";
   
+//buusan too ni 1 ees ylgaatai toglogchiin eeljiin onoog uurchlun nemegduulj eelj solin 
+if(diceNumber>1) {
+    roundscore = roundscore + diceNumber;
+    document.getElementById("current-"+Activplayer).textContent = roundscore ;
 }
-);
+
+     else {
+  Activplayer === 0 ? (Activplayer === 1 ) : ( Activplayer ===0 );
+     }
+});
